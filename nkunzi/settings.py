423735dafadf9841d7d6b5269cfe6b0d781ef9d4
Abigail <92498978+molusi@users.noreply.github.com>
2021-10-15 12:58:25 +0200

    Update settings.py

--- nkunzi/settings.py
+++ nkunzi/settings.py
@@ -60,7 +60,6 @@ ALLOWED_HOSTS = ['.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
