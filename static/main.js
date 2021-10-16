ade298e1df80620da765ba38fcd8be2248b305aa
Abigail <92498978+molusi@users.noreply.github.com>
2021-10-16 14:37:15 +0200

    Update main.js

--- static/main.js
+++ static/main.js
@@ -8,6 +8,21 @@ if(msg.contains(info)){
 setTimeout(()=>msg.remove(),5000)}
 });
 
+const myForm = document.querySelector("#my-form")
+const emailInput = document.querySelector("#email-input")
+const passwordInput = document.querySelector("#password-input")
+const mSg = document.querySelector("#mesg")
+
+
+myForm.addEventListener('submit',onSubbmit);
+
+function onSubmit(e){
+  e.preventDefault();
+  if(emailInput.value === '' || passwordInput.value ===''){
+  mSg.classList.add('is-danger');
+    mSg.innerHTML = "Please enter all fields"
+    setTimeout(()=>msg.remove(),3000)
+  };
 
 
 const control = document.querySelectorAll(".input");
