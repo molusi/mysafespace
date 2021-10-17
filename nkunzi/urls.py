ba2deacd85cd88052edbdc46828c76adb1f505a2
Abigail <92498978+molusi@users.noreply.github.com>
2021-10-17 02:32:17 +0200

    Update urls.py

--- nkunzi/urls.py
+++ nkunzi/urls.py
@@ -26,5 +26,7 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('accounts/', include('accounts.urls')),
     path('books/', include('books.urls')),]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
