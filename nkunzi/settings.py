a4480e7fc5bccfd69a08b2064c87610a0cdbfb3e
Abigail <92498978+molusi@users.noreply.github.com>
2021-10-17 03:08:27 +0200

    Update settings.py

--- nkunzi/settings.py
+++ nkunzi/settings.py
@@ -166,12 +166,12 @@ USE_TZ = True
 STATICFILES_STORAGE =  'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/staticfiles/'
-STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_DIR = os.path.join(BASE_DIR, 'staticfiles/media/')
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/staticfiles/media/'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
