5f73722e4557848a7392c6d3ae66b93e6e4f8370
Abigail <92498978+molusi@users.noreply.github.com>
2021-10-17 02:33:48 +0200

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 from . import views
 from .views import userprofileview,UserProfileUpdateView, MyPostsView, articlecreateview,ArticleUpdateView, ArticleDeleteView, ArticleDetailView, SearchView
