88d4b9de9dbff697d2e288bd51fef4eff277a150
Abigail <92498978+molusi@users.noreply.github.com>
2022-06-27 05:19:43 +0200

    Update settings.py

--- nkunzi/settings.py
+++ nkunzi/settings.py
@@ -51,7 +51,7 @@ SECRET_KEY = config("SECRET_KEY")
 # Raises Django's ImproperlyConfigured
 # exception if SECRET_KEY not in os.environ
 SECRET_KEY = config("SECRET_KEY")
-DEBUG = False
+DEBUG = True
 
 
 cloudinary.config(
